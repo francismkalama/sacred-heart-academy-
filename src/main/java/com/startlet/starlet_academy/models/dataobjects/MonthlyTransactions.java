6881e756cd11ee0e02ca518c93fed969d1f58e20
Arthur367 <arthurkin2019@gmail.com>
2024-12-17 19:28:43 +0300

    pulled changes

--- src/main/java/com/startlet/starlet_academy/models/dataobjects/MonthlyTransactions.java
+++ src/main/java/com/startlet/starlet_academy/models/dataobjects/MonthlyTransactions.java
@@ -1,19 +1,4 @@
 package com.startlet.starlet_academy.models.dataobjects;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
-
-@Data
-@Table( name = "monthly_transactions")
-@Entity
 public class MonthlyTransactions {
-    @Id
-    private String month;
-    private int year;
-    private double totalExpectedAmount;
-    private double totalOutstandingBalance;
-    private double totalAmountPaid;
-    // Constructors, Getters, and Setters
 }
